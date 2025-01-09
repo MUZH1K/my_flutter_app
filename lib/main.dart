9bec4ca982b90da9430567f592b82150666b8408
MUZH1K <SergeyDudin963782@gmail.com>
2025-01-09 18:56:58 +0300

    обернул MaterialApp в провайдер

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return RepositoryProvider(create: (context) => CounterRepository(),
+      child : MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
